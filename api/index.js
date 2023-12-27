f1f3ce02fd92502ab268c498e025144ceb9308e1
Priya <priyagajakoah1@gmail.com>
2023-12-27 17:43:07 +0530

    user api created

--- api/index.js
+++ api/index.js
@@ -1,14 +1,19 @@
-import express from 'express'
-import mongoose from 'mongoose';
-import dotenv from 'dotenv'
-dotenv.config()
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+dotenv.config();
 const app = express();
 
-mongoose.connect(process.env.MONGO).then(() => {
-    console.log('connected to mongodb')
-}).catch(err => {
-    console.log('failed to connect to db')
-})
-app.listen(3000,() => {
-    console.log('server is running on port 3000 -----')
-})
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log("failed to connect to db");
+  });
+app.use("/api/user", userRouter);
+app.listen(3000, () => {
+  console.log("server is running on port 3000 -----");
+});
