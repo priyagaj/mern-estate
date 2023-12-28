e16ef050457ce41f3123c6f04952918614b3ef1c
Priya <priyagajakoah1@gmail.com>
2023-12-28 14:21:14 +0530

    created middleware and function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,13 +1,14 @@
 import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs'
-export const signUp = async (req,res) => {
-    const { userName, email, password } = req.body;
-    const hashPassword = bcryptjs.hashSync(password,1);
-    try{
-
-        const newUser = new User({ userName, email, password: hashPassword });
-        await newUser.save();
-        res.status(201).json('new user created')
-    }catch(err){ res.status(500).json(err.message)}
-   
-}
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+export const signUp = async (req, res, next) => {
+  const { userName, email, password } = req.body;
+  const hashPassword = bcryptjs.hashSync(password, 1);
+  try {
+    const newUser = new User({ userName, email, password: hashPassword });
+    await newUser.save();
+    res.status(201).json("new user created");
+  } catch (err) {
+    next(err)
+  }
+};
--- api/index.js
+++ api/index.js
@@ -16,7 +16,16 @@ mongoose
   });
   app.use(express.json())
 app.use("/api/user", userRouter);
-app.use("/api/auth", authRouter)
+app.use("/api/auth", authRouter);
+app.use((err,req,res,next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server error";
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
 app.listen(3000, () => {
   console.log("server is running on port 3000 -----");
 });
