670fef6f3053c3bcc63293213d4688b29c47ba28
Priya <priyagajakoah1@gmail.com>
2023-12-28 20:31:23 +0530

    completed signuppage ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,23 +6,25 @@ export default function Header() {
   return (
     <header className='bg-slate-200 shadow-md'>
         <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+            <Link to='/'>
         <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
             <span className='text-slate-500'>Priya</span>
             <span className='text-slate-700'>Estate</span>
         </h1>
+            </Link>
         <form className='bg-slate-100  p-3 rounded-lg flex items-center'>
             <input type="text" className='bg-transparent outline-none w-24 sm:w-64'/>
             <FaSearch className='text-slate-600'/>
         </form>
         <ul className="flex gap-3">
             <Link to={'/'}>
-                <li>Home</li>
+                <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
             </Link>
             <Link to={'/about'}>
-                <li>About</li>
+                <li className='hidden sm:inline  text-slate-700 hover:underline'>About</li>
             </Link>
             <Link to={'/sign-in'}>
-                <li>Sign In</li>
+                <li className=' text-slate-700 hover:underline'>Sign In</li>
             </Link>
         </ul>
         </div>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
 export default function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold">Sign Up</h1>
+      <form action="" className="flex flex-col gap-4 p-3">
+        <input
+          type="text"
+          placeholder="username"
+          className="border rounded-lg p-3"
+        />
+        <input
+          type="text"
+          placeholder="email"
+          className="border rounded-lg p-3"
+        />
+        <input
+          type="text"
+          placeholder="password"
+          className="border rounded-lg p-3"
+        />
+        <button className="bg-slate-700 p-3 rounded-lg text-white hover:opacity-95 disabled:opacity-70 uppercase">
+          Sign Up
+        </button>
+      </form>
+      <div>
+        <p className="px-3">
+          Have an account ? 
+          <Link to={'/sign-in'}>
+          <span className="text-blue-700"> Sign In</span>
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 }
